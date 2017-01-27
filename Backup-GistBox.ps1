b470b76d4ad8e8366e416def4b32966be7b08172
Janik von Rotz <login@janikvonrotz.ch>
2017-01-27 12:57:39 +0100

    add metadata

--- Backup-GistBox.ps1
+++ Backup-GistBox.ps1
@@ -1,5 +1,6 @@
 $url = "https://api.github.com/users/janikvonrotz/gists?page="
 $root = "/Users/janikvonrotz/LocalDrive/GistBox"
+$Metadata = @()
 
 (1..11) | %{
 
@@ -14,6 +15,7 @@ $root = "/Users/janikvonrotz/LocalDrive/GistBox"
 
     $pullUrl = $_.git_pull_url
     $name = ($_.description).split("#")[0] -replace "`n","" -replace "`r","" -replace "`r`n","" -replace ":",""
+    $tags = ($_.description).split("#")[1..100] | %{"#$_" -replace "`n","" -replace "`r","" -replace "`r`n",""}
     $localPath = Join-Path $root $name
 
     if(Test-Path $localPath){
@@ -21,6 +23,7 @@ $root = "/Users/janikvonrotz/LocalDrive/GistBox"
      cd ($localPath)
      Write-Host "Update gist: $($name)"
      git pull
+     cd $root
 
     }else{
 
@@ -29,5 +32,9 @@ $root = "/Users/janikvonrotz/LocalDrive/GistBox"
      git clone $pullUrl "$localPath" --quiet
 
     }
+
+    $Metadata += @{name=$name;tags=$tags}
   }
 }
+
+$Metadata | ConvertTo-Json | Out-File Metadata.json -Encoding utf8
